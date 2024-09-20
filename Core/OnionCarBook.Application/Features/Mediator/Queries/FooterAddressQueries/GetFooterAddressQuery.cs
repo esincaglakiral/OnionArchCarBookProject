fb29c27f869026648e650085ce20691fb404fe07
Esin Çağla KIRAL <caglakiral@gmail.com>
2024-09-21 00:40:30 +0300

    FooterAdress class mediator operations

---
+++ Core/OnionCarBook.Application/Features/Mediator/Queries/FooterAddressQueries/GetFooterAddressQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using OnionCarBook.Application.Features.Mediator.Results.FooterAddressResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionCarBook.Application.Features.Mediator.Queries.FooterAddressQueries
+{
+    public class GetFooterAddressQuery : IRequest<List<GetFooterAddressQueryResult>>
+    {
+    }
+}
