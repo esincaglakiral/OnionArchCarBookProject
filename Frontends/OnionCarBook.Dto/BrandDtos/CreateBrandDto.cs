2f08451996c2b95c3eb26994f04fd12856f43f55
Esin Çağla KIRAL <caglakiral@gmail.com>
2024-09-25 04:51:22 +0300

    added dtos

---
+++ Frontends/OnionCarBook.Dto/BrandDtos/CreateBrandDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionCarBook.Dto.BrandDtos
+{
+    public class CreateBrandDto
+    {
+        public string Name { get; set; }
+
+    }
+}
---
+++ Frontends/OnionCarBook.Dto/PricingDtos/CreatePricingDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionCarBook.Dto.PricingDtos
+{
+    public class CreatePricingDto
+    {
+        public string Name { get; set; }
+
+    }
+}
