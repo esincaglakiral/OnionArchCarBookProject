5a36f18595c32f993aa83e1cf6cb3ee23713ccce
Esin Çağla KIRAL <caglakiral@gmail.com>
2024-10-08 02:26:27 +0300

    Bringing all blogs with author names mediator and api metot

--- Presentation/OnionCarBook.WebApi/Controllers/BlogsController.cs
+++ Presentation/OnionCarBook.WebApi/Controllers/BlogsController.cs
@@ -63,5 +63,12 @@ namespace OnionCarBook.WebApi.Controllers
             var values = await _mediator.Send(new GetLast3BlogsWithAuthorsQuery());
             return Ok(values);
         }
+
+        [HttpGet("GetAllBlogsWithAuthorList")]
+        public async Task<IActionResult> GetAllBlogsWithAuthorList()
+        {
+            var values = await _mediator.Send(new GetAllBlogsWithAuthorQuery());
+            return Ok(values);
+        }
     }
 }
