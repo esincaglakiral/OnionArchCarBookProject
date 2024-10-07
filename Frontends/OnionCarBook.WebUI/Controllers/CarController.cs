78f84bbcc30e04a31e2ea007080d4bb29b19f8f4
Esin Çağla KIRAL <caglakiral@gmail.com>
2024-10-07 04:59:41 +0300

    Fetching API with carpricing and consuming the API anf Frontends update

--- Frontends/OnionCarBook.WebUI/Controllers/CarController.cs
+++ Frontends/OnionCarBook.WebUI/Controllers/CarController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using OnionCarBook.Dto.CarDtos;
+using OnionCarBook.Dto.CarPricingDtos;
 
 namespace OnionCarBook.WebUI.Controllers
 {
@@ -18,11 +19,11 @@ namespace OnionCarBook.WebUI.Controllers
             ViewBag.v1 = "Araçlarımız";
             ViewBag.v2 = "Aracınızı Seçiniz";
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7077/api/Cars/GetCarWithBrand");
+            var responseMessage = await client.GetAsync("https://localhost:7077/api/CarPricings/GetCarPricingWithCarList");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<ResultCarWithBrandsDtos>>(jsonData);
+                var values = JsonConvert.DeserializeObject<List<ResultCarPricingWithCarDto>>(jsonData);
                 return View(values);
             }
             return View();
--- Infrastructure/OnionCarBook.Persistance/Repositories/CarPricingRepositories/CarPricingRepository.cs
+++ Infrastructure/OnionCarBook.Persistance/Repositories/CarPricingRepositories/CarPricingRepository.cs
@@ -23,6 +23,7 @@ namespace OnionCarBook.Persistance.Repositories.CarPricingRepositories
         {
             var values = _context.CarPricings.Include(x => x.Car).ThenInclude(y => y.Brand).Include(x => x.Pricing).Where(z => z.PricingID == 2).ToList();
             return values;
+            //pricing id =2 olanlar günlük fiyatlar oluyor dolayısıyla arçların günlük fiyatları listelenir burda
         }
     }
 }
